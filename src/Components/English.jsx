a5e0a482c6b0cdfa8b92fbce0df5827ea627f539
Khojiakbarbe <121534562+Khojiakbarbe@users.noreply.github.com>
2023-03-20 00:21:00 +0500

    smth added and smth changed

--- src/Components/English.jsx
+++ src/Components/English.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
+import img1 from '../images/English/img1.png'
+import img2 from '../images/English/img2.png'
+import img3 from '../images/English/img3.png'
+import img4 from '../images/English/img4.png'
 
 
 export default function English() {
@@ -9,10 +12,26 @@ export default function English() {
 
     return (
         <div className="container mb-5">
-            <div className="dasturlashKurslar row ">
-                <div className="col-md-6 pb-5">
+            <div className="englishCourse row ">
+                <div className="col-md-6">
+                    <img src={img1} alt="" />
+                </div>
+                <div className="col-md-6 p-5 responsiveMargin" style={{paddingLeft:'6%'}}>
+                    <h1>General English</h1>
+                    <h5>Dasturlash sohasi bo’yicha hech qanday bilimga ega bo’lmagan har qanday inson dasturlash saboqlarini o’rganishi mumkin</h5>
+
+                    <button className="batafsilBtn" onClick={() => navigate('/aloqa')}>Batafsil</button>
+                    <a href="https://forms.gle/o3JjMAxcQf1HyY3Z9">
+                        <button className="batafsilBtn" style={{ marginLeft: '2%' }}>
+                            Kursga yozilish
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <div className="englishCourse row ">
+                <div className="col-md-6 pb-5 text-start responsiveMargin" style={{paddingLeft:'6%'}}>
                     <h1>IELTS</h1>
-                    <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptates!</h5>
+                    <h5>Dasturlash sohasi bo’yicha hech qanday bilimga ega bo’lmagan har qanday inson dasturlash saboqlarini o’rganishi mumkin</h5>
 
                     <button className="batafsilBtn" onClick={() => navigate('/aloqa')}>Batafsil</button>
                     <a href="https://forms.gle/o3JjMAxcQf1HyY3Z9">
@@ -21,11 +40,17 @@ export default function English() {
                         </button>
                     </a>
                 </div>
+                <div className="col-md-6">
+                    <img src={img2} alt="" />
+                </div>
             </div>
-            <div className="dasturlashKurslar row text-center">
-                <div className="col-md-6 pb-5">
+            <div className="englishCourse row">
+                <div className="col-md-6">
+                    <img src={img3} alt="" />
+                </div>
+                <div className="col-md-6 pb-5 responsiveMargin" style={{paddingLeft:'6%'}}>
                     <h1>CEFR</h1>
-                    <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptates!</h5>
+                    <h5>Dasturlash sohasi bo’yicha hech qanday bilimga ega bo’lmagan har qanday inson dasturlash saboqlarini o’rganishi mumkin</h5>
 
                     <button className="batafsilBtn" onClick={() => navigate('/aloqa')}>Batafsil</button>
                     <a href="https://forms.gle/o3JjMAxcQf1HyY3Z9">
@@ -35,9 +60,8 @@ export default function English() {
                     </a>
                 </div>
             </div>
-            <div className="dasturlashKurslar row ">
-                <div className="col-md-6"></div>
-                <div className="col-md-6 pb-5">
+            <div className="englishCourse row ">
+                <div className="col-md-6 pb-5 text-start responsiveMargin" style={{paddingLeft:'6%'}}>
                     <h1>KIDS English</h1>
                     <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptates!</h5>
 
@@ -47,6 +71,9 @@ export default function English() {
                             Kursga yozilish
                         </button>
                     </a>
+                </div>
+                <div className="col-md-6">
+                    <img src={img4} alt="" />
                 </div>
             </div>
         </div>
